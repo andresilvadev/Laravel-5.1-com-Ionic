915f4ca078bf4802eb3b23f38daae9c1414a9da3
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-12-22 13:58:15 -0200

    Relacionando Product com Category

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CodeDelivery\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}

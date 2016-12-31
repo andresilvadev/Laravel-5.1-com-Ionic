b6717fb7d3b4e52b3de42ecdbce53b5a51887cac
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-12-31 14:22:00 -0200

    Inserindo title na view

--- ionic/www/js/controllers/home-controller.js
+++ ionic/www/js/controllers/home-controller.js
@@ -1,6 +1,6 @@
 angular.module('deliveryApp.controllers', [])
     .controller('HomeCtrl', function($scope, $state, $stateParams){
         $scope.nome = $stateParams.nome;    // Recuperando parametros de rota
-        $scope.state = $state.current;      // Mostranto o state atual 
+        $scope.state = $state.current;      // Mostranto o state atual
 
     });

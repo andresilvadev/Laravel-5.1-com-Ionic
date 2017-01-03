3d101043d84f0cf068d26a0f332ac28af9cba2f0
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2017-01-02 23:44:42 -0200

    Montando template de login

--- ionic/www/js/controllers/login.controller.js
+++ ionic/www/js/controllers/login.controller.js
@@ -1,4 +1,8 @@
 angular.module('deliveryApp.controllers',[])
     .controller('LoginCtrl',['$scope', function ($scope) {
+        $scope.login = function () {
+            alert('Login rodando');
+        }
+
         console.log('Testando controller login');
     }]);

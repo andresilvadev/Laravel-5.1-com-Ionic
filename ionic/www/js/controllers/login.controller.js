dc116b115b3d7db61fc2b6317423771b630e73f8
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2017-01-03 16:59:35 -0200

    Ajustando formulario de login

--- ionic/www/js/controllers/login.controller.js
+++ ionic/www/js/controllers/login.controller.js
@@ -1,21 +1,23 @@
 angular.module('deliveryApp.controllers',[])
-    .controller('LoginCtrl',['$scope','OAuth','$cookies', function ($scope,OAuth,$cookies) {
+    .controller('LoginCtrl',['$scope','OAuth','$cookies','$ionicPopup', function ($scope,OAuth,$cookies,$ionicPopup) {
 
         $scope.user = {     // Garantindo que os campos do formulário estejam vázios
             username: '',
             password: ''
-        }
+        };
 
         $scope.login = function () {
             OAuth.getAccessToken($scope.user)
-                .then(function (data) { // Se houver sucesso
-                console.log(data);                ;
+                .then(function (data) {     // Se houver sucesso
+                console.log(data);
                 console.log($cookies.getObject('token'));
 
             }, function (responseError) {    // Se houver fracasso
-
-            })
+                $ionicPopup.alert({
+                    title: 'Warning',
+                    template: 'Login ou Password invalid'
+                })
+            });
         }
 
-        console.log('Testando controller login');
     }]);
